b73d9e727f62945044bd68f62601900c41c1fae7
groverzhu <groverzhu@gmail.com>
2020-08-13 19:03:47 +0800

    feat(math):快速幂

---
+++ LeetCode/Math/pow.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * 计算x^n
+ */ 
+double pow(double x, int n) {
+    if (n == 0) return 1;
+    double result = 1.0;
+
+    // 快速幂求法
+    for (int i = n; i != 0; i /= 2) {
+        if (i % 2 != 0) {
+            result *= x;
+        }
+        x *= x;
+    }
+    return n > 0 ? result : 1.0 / result;
+}
+
+// 迭代求法
+double recursive_pow(double x, int n) {
+    if (n == 0) return 1.0;
+    double result = recursive_pow(x, n / 2);
+    if (n % 2 != 0) {
+        // 需要处理n<0的情况
+        if (n > 0) {
+            result = result * result * x;
+        } else {
+            result = (result * result) / x;
+        }
+    } else {
+        result = result * result;
+    }
+    return result;
+}
+
+int main() {
+    double x = 2;
+    int n = -2;
+    printf("%lf\n", recursive_pow(x, n));
+    return 0;
+}
