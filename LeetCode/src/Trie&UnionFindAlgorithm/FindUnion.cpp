5591ba165eb57e2721096d2f14c2e0699fb75063
groverzhu <groverzhu@gmail.com>
2022-01-10 20:53:44 +0800

    并查集 前缀树

--- LeetCode/src/Trie&UnionFindAlgorithm/FindUnion.cpp
+++ LeetCode/src/Trie&UnionFindAlgorithm/FindUnion.cpp
@@ -1,37 +1,37 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <numeric>
+#include <cassert>
+
 using namespace std;
 
+// 并查集两个通用函数
+int Find(vector<int>& nums, int i) {
+    int root = i;
+    while (nums[root] != root) root = nums[root];
+
+    while (nums[i] != i) {
+        int temp = i;
+        i = nums[i];
+        nums[temp] = root;
+    }
+    return root;
+}
+
+void Union(vector<int>& nums, int i, int j) {
+    int root_i = Find(nums, i);
+    int root_j = Find(nums, j);
+    nums[root_i] = root_j;
+}
+
+// 省份数量
+// https://leetcode-cn.com/problems/number-of-provinces/ 
 int findCircleNum(vector<vector<int>>& isConnected) {
-    int size = static_cast<int>(isConnected.size());
+    int size = isConnected.size();
     if (size <= 1) return size;
 
-    auto Find = [](vector<int>& nums, int j) {
-        int root = j;
-        while (nums[root] != root) {
-            root = nums[root];
-        }
-
-        while (nums[j] != j) {
-            int temp = j;
-            j = nums[j];
-            nums[temp] = root;
-
-        }
-
-        return root;
-    };
-
-    auto Union = [&Find](vector<int>& nums, int i, int j) {
-        int root_i = Find(nums, i);
-        int root_j = Find(nums, j);
-        nums[root_i] = nums[root_j];
-    };
-
     vector<int> disjoint(size, 0);
-    std::iota(disjoint.begin(), disjoint.end(), 0);
+    iota(disjoint.begin(), disjoint.end(), 0);
 
     for (int i = 0; i < size; i++) {
         for (int j = i + 1; j < size; j++) {
@@ -42,17 +42,126 @@ int findCircleNum(vector<vector<int>>& isConnected) {
     }
 
     int cnt = 0;
-    for (int i  = 0; i < size; i++) {
+    for (int i = 0; i < size; i++) {
         if (disjoint[i] == i) ++cnt;
     }
 
     return cnt;
 }
 
+// 岛屿数量
+// https://leetcode-cn.com/problems/number-of-islands/
+
+int numIslands(vector<vector<char>>& grid) {
+    int rows = grid.size();
+    int cols = grid[0].size();
+    int total = rows * cols;
+    vector<int> disjoint(total, 0);
+    iota(disjoint.begin(), disjoint.end(), 0);
+
+    const int LEN = 4;
+    constexpr int dx[LEN] = {0, -1, 0, 1};
+    constexpr int dy[LEN] = {1, 0, -1, 0};
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            if (grid[i][j] == '1') {
+                for (int k = 0; k < LEN; k++) {
+                    int x = i + dx[k];
+                    int y = j + dy[k];
+                    if (x >= 0 && x < rows && y >=0 && y < cols && grid[x][y] == '1') {
+                        Union(disjoint, i * cols + j, x * cols + y);
+                    }
+                }
+            }
+        }
+    }
+
+    int cnt = 0;
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            if (grid[i][j] == '1' && disjoint[i * cols + j] == (i * cols + j)) {
+                ++cnt;
+            }
+        }
+    }
+
+    return cnt;
+}
+
+// 被围绕的区域
+// https://leetcode-cn.com/problems/surrounded-regions/
+
+void solve(vector<vector<char>>& board) {
+    int rows = board.size();
+    int cols = board[0].size();
+    if (rows <= 1 || cols <= 1) return;
+
+    int total = rows * cols + 1;
+    int dummy = rows * cols; // 哑节点，用来连接外部'O'的位置
+
+    vector<int> disjoint(total, 0);
+    iota(disjoint.begin(), disjoint.end(), 0);
+
+    for (int i = 0; i < rows; i++) {
+        if (board[i][0] == 'O') Union(disjoint, i * cols, dummy);
+        if (board[i][cols - 1] == 'O') Union(disjoint, i * cols + cols - 1, dummy);
+    }
+
+    for (int j = 1; j < cols - 1; j++) {
+        if (board[0][j] == 'O') Union(disjoint, j, dummy);
+        if (board[rows - 1][j] == 'O') Union(disjoint, (rows - 1) * cols + j, dummy);
+    }
+
+    const int LEN = 4;
+    constexpr int dx[LEN] = {0, -1, 0, 1};
+    constexpr int dy[LEN] = {1, 0, -1, 0};
+    for (int i = 1; i < rows; i++) {
+        for (int j = 1; j < cols; j++) {
+            if (board[i][j] == 'O') {
+                for (int k = 0; k < LEN; k++) {
+                    int x = i + dx[k];
+                    int y = j + dy[k];
+                    if (x >= 0 && x < rows && y >= 0 && y < cols && board[x][y] == 'O') {
+                        Union(disjoint, i * cols + j, x * cols + y);
+                    }
+                }
+            }
+        }
+    }
+
+    for (int i = 1; i < rows; i++) {
+        for (int j = 1; j < cols; j++) {
+            if (board[i][j] == 'O' && Find(disjoint, i * cols + j) != Find(disjoint, dummy)) {
+                board[i][j] = 'X';
+            }
+        }
+    }
+}
 
 
-int main() {
-    vector<vector<int>> graph = {{1, 0, 0, 1, 0}, {0, 1, 1, 0, 1}, {0, 1, 1, 1, 0}, {1, 0, 1, 1, 0}, {0, 1, 1, 0, 1}};
-    cout << findCircleNum(graph) << endl;
+int main(int argc, char* argv[]) {
+    // 省份数量
+    vector<vector<int>> graph = {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
+    assert(2 == findCircleNum(graph));
+    graph = {{1,0,0},{0,1,0},{0,0,1}};
+    assert(3 == findCircleNum(graph));
+
+    // 岛屿数量
+    vector<vector<char>> grid = {{'1','1','1','1','0'}, {'1','1','0','1','0'}, {'1','1','0','0','0'}, {'0','0','0','0','0'}};
+    assert(1 == numIslands(grid));
+    grid = {{'1','1','0','0','0'}, {'1','1','0','0','0'}, {'0','0','1','0','0'}, {'0','0','0','1','1'}};
+    assert(3 == numIslands(grid));
+
+    // 被围绕的区域
+    vector<vector<char>> board = {{'X','X','X','X'},{'X','O','O','X'},{'X','X','O','X'},{'X','O','X','X'}};
+    vector<vector<char>> ans = {{'X','X','X','X'},{'X','X','X','X'},{'X','X','X','X'},{'X','O','X','X'}};
+    solve(board);
+    assert(board == ans);
+    board = {{'X'}};
+    ans = {{'X'}};
+    solve(board);
+    assert(board == ans);
     return 0;
 }
--- LeetCode/src/Trie&UnionFindAlgorithm/Trie.cpp
+++ LeetCode/src/Trie&UnionFindAlgorithm/Trie.cpp
@@ -1,29 +1,34 @@
 #include <iostream>
 #include <map>
 #include <vector>
+#include <set>
 #include <string>
 #include <queue>
 #include <cassert>
+#include <ctime>
 
 using namespace std;
 
+// 实现 Trie (前缀树) https://leetcode-cn.com/problems/implement-trie-prefix-tree/
 struct TrieNode {
     bool is_end;
     map<char, TrieNode*> next;
+
     TrieNode() {
         is_end = false;
         next = {};
     }
+
     ~TrieNode() {
         for (auto node : next) {
-            delete node.second;
+            if (!node.second) delete node.second;
         }
     }
 };
 
 class Trie {
-public:
 
+public:
     Trie() {
         root = new TrieNode();
     }
@@ -32,9 +37,9 @@ public:
         delete root;
     }
 
-    void insert(string s) {
-        auto node = this->root;
-        for (auto c : s) {
+    void insert(string word) {
+        auto node = root;
+        for (auto c : word) {
             if (node->next.find(c) == node->next.end()) {
                 node->next[c] = new TrieNode();
             }
@@ -43,55 +48,53 @@ public:
         node->is_end = true;
     }
 
-    bool search(string s) {
-        auto node = this->root;
-        for (auto c: s) {
-            if (node->next.find(c) == node->next.end()) return false;
+    bool search(string word) {
+        auto node = root;
+        for (auto c : word) {
+            if (node->next.find(c) == node->next.end()) {
+                return false;
+            }
             node = node->next[c];
         }
         return node->is_end;
     }
 
-    bool startWith(string s) {
-        auto node = this->root;
-        for (auto c : s) {
-            if (node->next.find(c) == node->next.end()) return false;
+    bool startsWith(string prefix) {
+        auto node = root;
+        for (auto c : prefix) {
+            if (node->next.find(c) == node->next.end()) {
+                return false;
+            }
             node = node->next[c];
         }
         return true;
     }
 
+    // 最长公共前缀 https://leetcode-cn.com/problems/longest-common-prefix/description/
     string longestCommonPrefix() {
-        // bfs 查找最长公共前缀
         if (!root || root->next.size() != 1) return "";
+        string prefix;
+        queue<pair<char, TrieNode*>> q;
+        q.push({'#', root});
 
-        queue<pair<TrieNode*, char>> q;
-        q.push({root, '#'});
-        string result;
         while (!q.empty()) {
             auto node = q.front();
             q.pop();
-            if (node.second != '#') {
-                result.push_back(node.second);
-            }
-
-            for (auto n : node.first->next) {
-                auto c = n.first;
-                auto ptr = n.second;
-                if (node.first->next.size() == 1 && !node.first->is_end) {
-                    q.push({ptr, c});
+            if (node.first != '#') prefix.push_back(node.first);
+            if (node.second->next.size() == 1 && !node.second->is_end) {
+                for (auto n : node.second->next) {
+                    q.push({n.first, n.second});
                 }
             }
         }
-
-        return result;
+        return prefix;
     }
 
 private:
     TrieNode* root;
-
 };
 
+// 最长公共前缀 https://leetcode-cn.com/problems/longest-common-prefix/description/
 string longestCommonPrefix(vector<string>& strs) {
     Trie trie;
     for (auto str : strs) {
@@ -100,8 +103,89 @@ string longestCommonPrefix(vector<string>& strs) {
     return trie.longestCommonPrefix();
 }
 
+// 单词搜索 II https://leetcode-cn.com/problems/word-search-ii/ 在该Trie结构下，leetcode会超时
+void dfs(vector<vector<char>>& board, Trie& trie, set<string>& result, string& temp, int x, int y, int level) {
+    if (board[x][y] < 'a') return;
+    if (trie.search(temp)) {
+        result.insert(temp);
+    }
+
+    board[x][y] -= 'a';
+
+    const int LEN = 4;
+    constexpr int dx[LEN] = {0, -1, 0, 1};
+    constexpr int dy[LEN] = {1, 0, -1, 0};
+    int rows = board.size();
+    int cols = board[0].size();
+    for (int k = 0; k < LEN; k++) {
+        int next_x = x + dx[k];
+        int next_y = y + dy[k];
+
+        if (next_x >= 0 && next_x < rows && next_y >= 0 && next_y < cols && board[next_x][next_y] >= 'a' && level < 10) {
+            temp.push_back(board[next_x][next_y]);
+            dfs(board, trie, result, temp, next_x, next_y, level + 1);
+            temp.pop_back();
+        }
+    }
+
+    board[x][y] += 'a'; 
+}
+
+vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
+    Trie trie;
+    for (auto str : words) {
+        trie.insert(str);
+    }
+
+    set<string> result;
+    int rows = board.size();
+    int cols = board[0].size();
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            string temp = "";
+            dfs(board, trie, result, temp, i, j, 0);
+        }
+    }
+    return vector<string>(result.begin(), result.end());
+}
+
+
 int main(int argc, char* argv[]) {
-    vector<string> strs = {"d", "daa", "da"};
-    cout << longestCommonPrefix(strs) << endl;
+    Trie trie1;
+    trie1.insert("apple");
+    assert(true == trie1.search("apple"));
+    assert(false == trie1.search("app"));
+    assert(true == trie1.startsWith("app"));
+    trie1.insert("app");
+    assert(true == trie1.search("app"));
+
+    vector<string> strs = {"flower","flow","flight"};
+    string ans = "fl";
+    assert(ans == longestCommonPrefix(strs));
+    strs = {"dog","racecar","car"};
+    ans = "";
+    assert(ans == longestCommonPrefix(strs));
+    strs = {"", "b"};
+    ans = "";
+    assert(ans == longestCommonPrefix(strs));
+    strs = {"abc", "abc", "abc"};
+    ans = "abc";
+    assert(ans == longestCommonPrefix(strs));
+
+    vector<vector<char>> board = {{'o','a','a','n'},{'e','t','a','e'},{'i','h','k','r'},{'i','f','l','v'}};
+    vector<string> words = {"oath","pea","eat","rain"};
+    vector<string> results = {"eat", "oath"};
+    auto tmp = findWords(board, words);
+    assert(results == tmp);
+
+    board = {{'b','d','a','a','c','c','b','e'},{'d','c','e','b','b','e','e','b'},{'b','d','b','e','b','a','a','e'},{'b','c','d','a','d','d','a','c'},{'e','d','b','c','a','d','a','c'},{'e','b','a','a','c','d','c','d'},{'d','e','c','c','b','d','d','c'},{'c','a','c','e','c','b','d','c'},{'a','e','d','b','c','b','a','a'},{'e','a','a','a','a','c','c','b'},{'d','e','e','e','c','b','c','e'}};
+    words = {"cbcccec"};
+    results = {"cbcccec"};
+    // auto begin = time(0);
+    tmp = findWords(board, words);
+    // auto end = time(0);
+    // cout << end - begin << endl;
+    assert(results == tmp);
+    
     return 0;
 }
