7f1fa0febd8ae4d73bcb4b94448efd8298403869
groverzhu <groverzhu@gmail.com>
2022-01-25 11:06:50 +0800

    climb stairs and two sum

---
+++ LeetCode/src/Array&List/ClimbingStairs.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <vector>
+
+#include <ctime>
+#include <cstdlib>
+#include <cassert>
+
+using namespace std;
+
+// https://leetcode-cn.com/problems/climbing-stairs/
+
+int climbStairsRecursive(int n) {
+    if (n <= 0) return 0;
+    if (n == 1) return 1;
+    if (n == 2) return 2;
+    return climbStairsRecursive(n - 1) + climbStairsRecursive(n - 2);
+}
+
+int climbStairsIteration(int n) {
+    if (n <= 0) return 0;
+    if (n == 1) return 1;
+    if (n == 2) return 2;
+    int dp[n + 1];
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 2;
+    for (int i = 3; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    return dp[n];
+}
+
+int climbStairs(int n) {
+    if (n <= 0) return 0;
+    if (n == 1) return 1;
+    if (n == 2) return 2;
+    
+    int first = 1;
+    int second = 2;
+
+    n -= 1;
+    while (--n) {
+        int tmp = second;
+        second = first + second;
+        first = tmp;
+    }
+    
+    return second;
+}
+
+int main(int argc, char* argv[]) {
+
+    int n = 1;
+    int ans1 = climbStairsRecursive(n);
+    int ans2 = climbStairsIteration(n);
+    int ans3 = climbStairs(n);
+
+    assert(ans1 == ans2);
+    assert(ans2 == ans3);
+    
+    clock_t begin, end;
+
+    n = 20;
+    begin = clock();
+    ans1 = climbStairsRecursive(n);
+    end = clock();
+    cout << "Recursive time cost: " << (end - begin) / CLOCKS_PER_SEC * 1000.0 << "ms\t";
+
+    begin = clock();
+    ans2 = climbStairsIteration(n);
+    end = clock();
+    cout << "Iteration time cost: " << (end - begin) / CLOCKS_PER_SEC * 1000.0 << "ms\t";
+
+    begin = clock();
+    ans3 = climbStairs(n);
+    end = clock();
+    cout << "Climb Stairs time cost: " << 1.0 * (end - begin) / CLOCKS_PER_SEC * 1000.0 << "ms" << endl;
+
+    assert(ans1 == ans2);
+    assert(ans2 == ans3);
+
+    n = 45;
+    begin = clock();
+    ans1 = climbStairsRecursive(n);
+    end = clock();
+    cout << "Recursive time cost: " << (end - begin) / CLOCKS_PER_SEC * 1000.0 << "ms\t";
+
+    begin = clock();
+    ans2 = climbStairsIteration(n);
+    end = clock();
+    cout << "Iteration time cost: " << (end - begin) / CLOCKS_PER_SEC * 1000.0 << "ms\t";
+
+    begin = clock();
+    ans3 = climbStairs(n);
+    end = clock();
+    cout << "Climb Stairs time cost: " << 1.0 * (end - begin) / CLOCKS_PER_SEC * 1000.0 << "ms" << endl;
+
+    assert(ans1 == ans2);
+    assert(ans2 == ans3);
+    
+    return 0;
+}
---
+++ LeetCode/src/Array&List/TwoSum.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <cassert>
+
+using namespace std;
+
+// https://leetcode-cn.com/problems/two-sum/
+vector<int> twoSum(vector<int>& nums, int target) {
+    map<int, int> val2index;
+    int size = static_cast<int>(nums.size());
+
+
+    for (int i = 0; i < size; i++) {
+        if (val2index.find(target - nums[i]) != val2index.end()) {
+            return {val2index[target - nums[i]], i};
+        }
+        val2index[nums[i]] = i;
+    }
+
+    return {};
+}
+
+int main(int argc, char* argv[]) {
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    auto result = twoSum(nums, target);
+    assert(result.size() == 2);
+    assert(target == nums[result[0]] + nums[result[1]]);
+    return 0;
+}
