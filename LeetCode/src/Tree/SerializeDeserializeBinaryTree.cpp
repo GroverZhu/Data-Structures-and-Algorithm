fbf6bc88cbc29a4929deced14c5fb1bbf66b3956
groverzhu <groverzhu@gmail.com>
2022-02-12 10:12:07 +0800

    feat(tree): 树的序列化

---
+++ LeetCode/src/Tree/SerializeDeserializeBinaryTree.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <cassert>
+
+#include "Tree/BinaryTreeNode.h"
+
+using namespace std;
+
+string serialize(TreeNode* root) {
+    if (!root) return "#";
+    // 前序遍历
+    string result = to_string(root->val) + ",";
+    result += serialize(root->left) + ",";
+    result += serialize(root->right);
+
+    return result;
+}
+
+TreeNode* deserializeHelper(string& data) {
+    if (data[0] == '#') {
+        if (data.size() > 2) {
+            data = data.substr(2);
+        }
+        return nullptr;
+    }
+    int pos = data.find(',');
+    int val = stoi(data.substr(0, pos));
+    data = data.substr(pos + 1);
+    TreeNode* root = new TreeNode(val);
+    root->left = deserializeHelper(data);
+    root->right = deserializeHelper(data);
+    return root;
+}
+
+TreeNode* deserialize(string data) {
+    return deserializeHelper(data);
+}
+
+// https://leetcode-cn.com/problems/serialize-and-deserialize-binary-tree/
+int main(int argc, char* argv[]) {
+    string data = "1,2,#,#,3,4,#,#,5,#,#";
+    auto root = deserialize(data);
+    auto result = serialize(root);
+    assert(result == data);
+    destroyTree(root);
+
+    data = "12,22,#,#,31,444,#,#,50,#,#";
+    root = deserialize(data);
+    result = serialize(root);
+    assert(result == data);
+    destroyTree(root);
+
+    return 0;
+}
---
+++ LeetCode/src/Tree/SerializeDeserializeNAryTree.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <cassert>
+
+using namespace std;
+
+class Node {
+
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {
+
+    }
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+
+};
+
+// https://leetcode-cn.com/problems/serialize-and-deserialize-n-ary-tree/
+
+string serialize(Node* root) {
+    string result = "";
+    if (root) {
+        result += to_string(root->val) + ",";
+        if (root->children.size() > 0) {
+            result += "[";
+            for (auto child : root->children) {
+                result += serialize(child);
+            }
+            result += "]";
+        }
+    }
+    return result;
+}
+
+Node* deserialize(string data) {
+    if (data.size() == 0) return nullptr;
+    Node* root = nullptr;
+    Node* cur = nullptr;
+    stack<Node*> stk;
+    while (!data.empty()) {
+        if (data[0] == '[') {
+            data = data.substr(1);
+            stk.push(cur);
+        } else if (data[0] == ']') {
+            data = data.substr(1);
+            stk.pop();
+        } else {
+            int index = data.find(',');
+            int val = stoi(data.substr(0, index));
+            data = data.substr(index + 1);
+            Node* node = new Node(val);
+
+            if (!root) {
+                root = node;
+            } else {
+                Node* parent = stk.top();
+                parent->children.push_back(node);
+            }
+            cur = node;
+        }
+    }
+    return root;
+}
+
+void destroyNAryTree(Node* root) {
+    if (!root) return;
+    for (auto child : root->children) {
+        destroyNAryTree(child);
+    }
+    delete root;
+    root = nullptr;
+}
+
+int main(int argc, char* argv[]) {
+    string data = "1,[2,3,[6,7,[11,[14,]]]4,[8,[12,]]5,[9,[13,]10,]]";
+    auto root = deserialize(data);
+    auto result = serialize(root);
+    assert(data == result);
+    destroyNAryTree(root);
+
+    data = "1,[3,[5,6,]2,4,]";
+    root = deserialize(data);
+    result = serialize(root);
+    assert(data == result);
+    destroyNAryTree(root);
+    return 0;
+}
