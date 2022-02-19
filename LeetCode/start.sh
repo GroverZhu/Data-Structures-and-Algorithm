f1bd298bc1ebbe53edcb5660d0b8c974e304ed77
groverzhu <groverzhu@gmail.com>
2022-02-19 16:05:51 +0800

    chore(run): 添加自动运行所有程序

--- LeetCode/start.sh
+++ LeetCode/start.sh
@@ -35,6 +35,29 @@ if [[ $1 = "release" ]]; then
     cd cmake-build-release && cmake .. && make -j 4
 fi
 
+# 测试release版本代码
+if [[ $1 = "run" ]]; then
+
+    if [[ ! -d "cmake-build-release" ]]; then
+        echo execution not exits!
+        exit 1;
+    fi
+
+    cd cmake-build-release
+    for file in ` ls `
+    do 
+        if [[ -f ${file} && -x ${file} ]]; then
+            echo "run ${file}......"
+            ./$file
+            if [[ $? -ne 0 ]]; then
+                echo "run ${file} failed!"
+                exit 1
+            fi
+        fi
+    done
+
+    echo "all tests passed!"
+fi
 
 if [[ $1 && $1 = "clean" ]]; then
     rm -rf cmake-build-*
