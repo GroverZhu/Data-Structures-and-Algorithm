249f91ec0d6f71caf6088f03bee2291d6d210247
groverzhu <groverzhu@gmail.com>
2022-02-19 16:06:57 +0800

    feat(DFS/BFS): 单词搜索，成语接龙

---
+++ LeetCode/src/DFS&BFS/WordLadder.cpp
@@ -0,0 +1,149 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+#include <queue>
+#include <climits>
+#include <cassert>
+
+using namespace std;
+
+// https://leetcode-cn.com/problems/word-ladder/
+int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+    set<string> cache(wordList.begin(), wordList.end());
+    if (cache.find(endWord) == cache.end()) return 0;
+
+    set<string> begin;
+    set<string> end;
+    set<string> visited;
+
+    begin.insert(beginWord);
+    end.insert(endWord);
+    visited.insert(beginWord);
+    visited.insert(endWord);
+
+    int step = 1;
+
+    while (!begin.empty() && !end.empty()) {
+        set<string> tmp;
+        for (auto str : begin) {
+            for (auto& c : str) {
+                auto reversed = c;
+                for (char t = 'a'; t <= 'z'; t++) {
+                    if (t == c) continue;
+                    c = t;
+                    if (end.find(str) != end.end()) return step + 1;
+                    if (visited.find(str) == visited.end() && cache.find(str) != cache.end()) {
+                        tmp.insert(str);
+                        visited.insert(str);
+                    }
+                }
+                c = reversed;
+            }
+        }
+        swap(begin, tmp);
+        if (begin.size() > end.size()) swap(begin, end);
+        ++step;
+    }
+
+    return 0;
+}
+
+// https://leetcode-cn.com/problems/word-ladder-ii/
+vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
+    set<string> cache(wordList.begin(), wordList.end());
+    vector<vector<string>> result;
+    if (cache.find(endWord) == cache.end()) return result;
+
+    queue<vector<string>> q;
+    q.push({beginWord});
+    set<string> visited;
+    int level = 1;
+    int minStep = INT_MAX;
+
+    while (!q.empty()) {
+        auto path = q.front();
+        q.pop();
+
+        if (static_cast<int>(path.size()) > level) {
+            for (auto str : visited) cache.erase(str);
+            visited.clear();
+
+            if (static_cast<int>(path.size()) > minStep) {
+                break;
+            } else {
+                level = static_cast<int>(path.size());
+            }
+        }
+
+        auto str = path.back();
+        for (auto& base : str) {
+            auto reversed = base;
+            for (int c = 'a'; c <= 'z'; c++) {
+                if (base == c) continue;
+                base = c;
+                if (cache.find(str) != cache.end()) {
+                    auto newPath = path;
+                    newPath.emplace_back(str);
+                    if (str == endWord) {
+                        result.push_back(newPath);
+                        minStep = level;
+                    } else {
+                        q.push(newPath);
+                        visited.insert(str);
+                    }
+                }
+            }
+            base = reversed;
+        }
+    }
+
+    return result;
+}
+
+int main(int argc, char* argv[]) {
+    string beginWord, endWord;
+    vector<string> wordList;
+    vector<vector<string>> lists;
+    set<vector<string>> ans_set;
+    set<vector<string>> result_set;
+
+    beginWord = "hit";
+    endWord = "cog";
+    wordList = {"hot","dot","dog","lot","log","cog"};
+    assert(5 == ladderLength(beginWord, endWord, wordList));
+    lists = findLadders(beginWord, endWord, wordList);
+    result_set = set<vector<string>>(lists.begin(), lists.end());
+    ans_set = {{"hit","hot","dot","dog","cog"}, {"hit","hot","lot","log","cog"}};
+    assert(ans_set == result_set);
+
+    beginWord = "hit";
+    endWord = "cog";
+    wordList = {"hot","dot","dog","lot","log"};
+    assert(0 == ladderLength(beginWord, endWord, wordList));
+    lists = findLadders(beginWord, endWord, wordList);
+    result_set = set<vector<string>>(lists.begin(), lists.end());
+    ans_set = {};
+    assert(ans_set == result_set);
+
+    beginWord = "hot";
+    endWord = "dog";
+    wordList = {"hot","dog"};
+    assert(0 == ladderLength(beginWord, endWord, wordList));
+    lists = findLadders(beginWord, endWord, wordList);
+    result_set = set<vector<string>>(lists.begin(), lists.end());
+    ans_set = {};
+    assert(ans_set == result_set);
+
+    beginWord = "red";
+    endWord = "tax";
+    wordList = {"ted","tex","red","tax","tad","den","rex","pee"};
+    assert(4 == ladderLength(beginWord, endWord, wordList));
+    lists = findLadders(beginWord, endWord, wordList);
+    result_set = set<vector<string>>(lists.begin(), lists.end());
+    ans_set = {{"red","ted","tad","tax"},{"red","ted","tex","tax"},{"red","rex","tex","tax"}};
+    assert(ans_set == result_set);
+
+
+    return 0;
+}
---
+++ LeetCode/src/DFS&BFS/WordSearch.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+#include <cassert>
+
+using namespace std;
+
+
+bool dfs(vector<vector<char>>& board, vector<vector<bool>>& visited, string word, int index, int x, int y) {
+    if (index == static_cast<int>(word.size()) - 1) {
+        return true;
+    }
+
+    constexpr int size = 4;
+    constexpr int dx[size] = {0, -1, 0, 1};
+    constexpr int dy[size] = {1, 0, -1, 0};
+    int rows = static_cast<int>(board.size());
+    int cols = static_cast<int>(board[0].size());
+
+    for (int i = 0; i < size; i++) {
+        int nextX = x + dx[i];
+        int nextY = y + dy[i];
+
+        if (nextX >= 0 && nextX < rows && nextY >= 0 && nextY < cols && !visited[nextX][nextY] && board[nextX][nextY] == word[index + 1]) {
+            visited[nextX][nextY] = true;
+            auto result = dfs(board, visited, word, index + 1, nextX, nextY);
+            if (result) return true;
+            visited[nextX][nextY] = false;
+        }
+    }
+
+    return false; 
+}
+
+// https://leetcode-cn.com/problems/word-search/
+bool exist(vector<vector<char>>& board, string word) {
+    int rows = static_cast<int>(board.size());
+    int cols = static_cast<int>(board[0].size());
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            if (board[i][j] == word[0]) {
+                vector<vector<bool>> visited(rows, vector<bool>(cols, false));
+                visited[i][j] = true;
+                auto result = dfs(board, visited, word, 0, i, j);
+                if (result) return result;
+            }
+        }
+    }
+
+    return false;
+}
+
+int main() {
+
+    vector<vector<char>> board;
+    string word;
+
+    board = {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
+    word = "ABCCED";
+    assert(true == exist(board, word));
+
+    board = {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
+    word = "SEE";
+    assert(true == exist(board, word));
+
+    board = {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
+    word = "ABCB";
+    assert(false == exist(board, word));
+
+    return 0;
+}
