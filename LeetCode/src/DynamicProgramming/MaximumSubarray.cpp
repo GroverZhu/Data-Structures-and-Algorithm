f010309bb5d222c9834bff5686b475663657e8b3
groverzhu <groverzhu@gmail.com>
2022-02-28 20:50:15 +0800

    test(dp): 在最大子数组和中添加判断子序列代码，增加多子序列结果的测例

--- LeetCode/src/DynamicProgramming/MaximumSubarray.cpp
+++ LeetCode/src/DynamicProgramming/MaximumSubarray.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <vector>
 #include <deque>
+#include <numeric>
 #include <cassert>
 
 using namespace std;
@@ -23,7 +24,7 @@ int maxSubArray(vector<int>& nums) {
     return result;
 }
 
-// 进阶版本，求其组成的数组
+// 进阶版本，求其组成的任意一个数组
 vector<int> maxSubArrayList(vector<int>& nums) {
     int size = nums.size();
     if (size <= 1) return nums;
@@ -33,8 +34,9 @@ vector<int> maxSubArrayList(vector<int>& nums) {
     int end = 0;
     int max_sum = sums[0];
     for (int i = 1; i < size; i++) {
+        // 第一次出现的最大子数组和
         sums[i] = max(sums[i - 1] + nums[i], nums[i]);
-        if (max_sum < sums[i]) {
+        if (max_sum < sums[i]) { // < 第一次出现的(最短)，<= 最后一次出现的(最长)
             max_sum = sums[i];
             end = i;
         }
@@ -52,30 +54,63 @@ vector<int> maxSubArrayList(vector<int>& nums) {
     return vector<int>(result.begin(), result.end());
 }
 
+// 判断一个数组(sub)是否为另一个数组(seq)的子序列
+bool isSubsequence(vector<int>& seq, vector<int>& sub) {
+    int seqSize = static_cast<int>(seq.size());
+    int subSize = static_cast<int>(sub.size());
+    if (subSize > seqSize) return false;
+    int seqIndex = 0;
+    int subIndex = 0;
+
+    while (seqIndex < seqSize && subIndex < subSize) {
+        while (seqIndex < seqSize && seq[seqIndex] != sub[subIndex]) {
+            seqIndex++;
+        }
+        if (seqIndex != seqSize) {
+            seqIndex++;
+            subIndex++;
+        }
+    }
+
+    return seqIndex <= seqSize && subIndex == subSize;
+}
+
 int main(int argc, char* argv[]) {
     vector<int> nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
     assert(6 == maxSubArray(nums));
     auto arr = maxSubArrayList(nums);
-    vector<int> ans = {4, -1, 2, 1};
-    assert(arr == ans);
+    assert(6 == accumulate(arr.begin(), arr.end(), 0));
+    assert(true == isSubsequence(nums, arr));
+    // vector<int> ans = {4, -1, 2, 1};
 
     nums = {1};
     assert(1 == maxSubArray(nums));
     arr = maxSubArrayList(nums);
-    ans = {1};
-    assert(ans == arr);
+    assert(1 == accumulate(arr.begin(), arr.end(), 0));
+    assert(true == isSubsequence(nums, arr));
+    // ans = {1};
 
 
     nums = {5, 4, -1, 7, 8};
     assert(23 == maxSubArray(nums));
     arr = maxSubArrayList(nums);
-    ans = {5, 4, -1, 7, 8};
-    assert(nums == arr);
+    assert(23 == accumulate(arr.begin(), arr.end(), 0));
+    assert(true == isSubsequence(nums, arr));
+    // ans = {5, 4, -1, 7, 8};
 
     nums = {-2, -1};
     assert(-1 == maxSubArray(nums));
     arr = maxSubArrayList(nums);
-    ans = {-1};
-    assert(arr == ans);
+    assert(-1 == accumulate(arr.begin(), arr.end(), 0));
+    assert(true == isSubsequence(nums, arr));
+    // ans = {-1};
+    
+    nums = {5, 4, -1, -1, 2};
+    assert(9 == maxSubArray(nums));
+    arr = maxSubArrayList(nums);
+    assert(9 == accumulate(arr.begin(), arr.end(), 0));
+    assert(true == isSubsequence(nums, arr));
+    // ans = {5, 4} {5, 4, -1, -1, 2}
+
     return 0;
 }
