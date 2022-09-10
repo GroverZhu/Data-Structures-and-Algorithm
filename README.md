18110e5cb907697e24550e008a8b442f141047e2
zhuriyong <hogan.zhu@qq.com>
2022-09-10 20:00:56 +0800

    feat(prefixSum&Diff): add discrete prefix sum and state compress prefix sum

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Data Structures and Algorithm
 
 Record the data structures and algorithm jounery, the cpskill move to this [repo](https://github.com/GroverZhu/CPSKILL)
+
+The `build_support`, `.github`, `CmakeLists.txt` is modify from [bustub](https://github.com/cmu-db/bustub)
--- src/PrefixSum&Diff/CarPooling.cpp
+++ src/PrefixSum&Diff/CarPooling.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <map>
 #include <vector>
 
 #include <cassert>
@@ -7,6 +8,31 @@ using namespace std;
 
 // https://leetcode-cn.com/problems/car-pooling/
 bool carPooling(vector<vector<int>>& trips, int capacity) {
+#if 1
+    int size = static_cast<int>(trips.size());
+    map<int, int> diff;
+
+    for (int i = 0; i < size; i++) {
+        int begin = trips[i][1];
+        int end = trips[i][2];
+        int passenger = trips[i][0];
+        diff[begin] += passenger;
+        diff[end] -= passenger;
+    }
+
+    int sum = 0;
+    for (auto& iter : diff) {
+        sum += iter.second;
+        iter.second = sum;
+        if (sum > capacity) {
+            return false;
+        }
+    }
+
+    return true;
+#endif
+
+#if 0
     int size = 0;
     for (auto trip : trips) {
         size = max(size, trip[2]);
@@ -28,6 +54,38 @@ bool carPooling(vector<vector<int>>& trips, int capacity) {
         if (num > capacity) return false;
     }
     return true;
+#endif
+}
+
+// https://leetcode.cn/problems/number-of-flowers-in-full-bloom/
+vector<int> fullBloomFlowers(vector<vector<int>>& flowers, vector<int>& persons) {
+    int len = static_cast<int>(flowers.size());
+    map<int, int> diff;
+
+    for (int i = 0; i < len; i++) {
+        assert(flowers[i].size() == 2);
+        diff[flowers[i][0]] += 1;
+        diff[flowers[i][1] + 1] -= 1;
+    }
+
+    int size = static_cast<int>(persons.size());
+
+    for (int i = 0; i < size; i++) {
+        diff[persons[i]] += 0;  // 将 persons[i] 到达的位置记录下来
+    }
+
+    int sum = 0;
+    for (auto& iter : diff) {
+        sum += iter.second;
+        iter.second = sum;
+    }
+
+    vector<int> ans(size, 0);
+    for (int i = 0; i < size; i++) {
+        ans[i] = diff[persons[i]];
+    }
+
+    return ans;
 }
 
 int main(int argc, char* argv[]) {
@@ -50,6 +108,11 @@ int main(int argc, char* argv[]) {
     trips = {{7, 5, 6}, {6, 7, 8}, {10, 1, 6}};
     capactity = 16;
     assert(false == carPooling(trips, capactity));
+
+    vector<vector<int>> flowers = {{1, 6}, {3, 7}, {9, 12}, {4, 13}};
+    vector<int> persons = {2, 3, 7, 11};
+    vector<int> answer = {1, 2, 2, 2};
+    assert(answer == fullBloomFlowers(flowers, persons));
 
     return 0;
 }
