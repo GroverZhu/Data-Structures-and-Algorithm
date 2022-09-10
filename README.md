33ab88f70bc2e2c2a8d2015d9c6a7cfb085cd02b
groverzhu <groverzhu@gmail.com>
2022-09-10 15:00:52 +0800

    factor(*): support github action and move cpskill code

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # Data Structures and Algorithm
 
-此项目记录刷题过程
-
-* [CPSKILL](./CPSKILL/)文件下为西北工业大学[C/C++](http://noj.nwpu.edu.cn/cpbox/)课程题目
-* [LeetCode](./LeetCode/) [🔗](https://leetcode-cn.com/problemset/all/)
+Record the data structures and algorithm jounery, the cpskill code will refactor soon.
---
+++ build_support/run_executable.sh
@@ -0,0 +1,13 @@
+for file in ` ls `
+    do 
+        if [[ -f ${file} && -x ${file} ]]; then
+            echo "In debug mode run ${file}......"
+            ./$file
+            if [[ $? -ne 0 ]]; then
+                echo "In debug mode run ${file} failed!"
+                exit 1
+            fi
+        fi
+    done
+
+echo "In debug mode all tests passed!"
