91003eff3e053b026eff3369542fc7fc839c6c86
GroverZhu <groverzhu@gmail.com>
2024-11-23 21:16:10 +0800

    feat(LongestValidParentheses): use stack to slove this problem

--- src/DynamicProgramming/LongestValidParentheses.cpp
+++ src/DynamicProgramming/LongestValidParentheses.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stack>
 #include <string>
 #include <vector>
 
@@ -8,6 +9,29 @@ using namespace std;
 
 // https://leetcode-cn.com/problems/longest-valid-parentheses/
 int longestValidParentheses(string s) {
+#if 1
+    int size = static_cast<int>(s.size());
+    stack<int> stk;
+
+    stk.push(-1);
+    int result = 0;
+    for (int i = 0; i < size; i++) {
+        if (s[i] == '(') {
+            stk.push(i);
+        } else {
+            stk.pop();
+            if (stk.empty()) {
+                stk.push(i);
+            } else {
+                result = max(result, i - stk.top());
+            }
+        }
+    }
+
+    return result;
+
+#endif
+#if 0
     int size = static_cast<int>(s.size());
     if (size < 2) return 0;
     vector<int> lens(size, 0);
@@ -24,6 +48,7 @@ int longestValidParentheses(string s) {
     }
 
     return result;
+#endif
 }
 
 int main(int argc, char* argv[]) {
